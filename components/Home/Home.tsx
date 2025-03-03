5b9abf442fb9e2fd92f0ed84099e9b0ffbe17823
N. Bhanu Durga Prasad <n.bhanudurgaprasad2209@gmail.com>
2025-03-03 22:11:26 +0530

    5th commit Feature Section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./Hero/Hero";
 import WhyChoose from "./WhyChoose/WhyChoose";
 import AnalyticsFeature from "./AnalyticsFeature/AnalyticsFeature";
+import Feature from "./Feature/Feature";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <WhyChoose />
       <AnalyticsFeature />
+      <Feature />
     </div>
   );
 };
